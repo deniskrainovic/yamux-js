0622dfd12536d2804cb5ef6e47278a371e62a878
deniskrainovicfaber <d.krainovic@faber.at>
2026-02-12 10:34:46 +0100

    chore: include compiled lib/ for git-based installs

---
+++ lib/header.d.ts
@@ -0,0 +1,11 @@
+export declare class Header {
+    version: number;
+    type: number;
+    flags: number;
+    streamID: number;
+    length: number;
+    static LENGTH: number;
+    constructor(version: number, type: number, flags: number, streamID: number, length: number);
+    static parse(buffer: Buffer): Header;
+    encode(): Buffer;
+}
---
+++ lib/server.d.ts
@@ -0,0 +1,6 @@
+import { Duplex } from 'stream';
+import { Config } from './mux';
+import { Session } from './session';
+export declare class Server extends Session {
+    constructor(onStream: (duplex: Duplex) => void, config?: Config);
+}
